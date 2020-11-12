d58f88acaf9050b9546e36f2a26cad4c404701eb
tatiana <khen.tatiana@gmail.com>
2020-11-11 23:55:21 -0800

    Launching camera functionality is finished

--- ParentAid/CameraViewController.swift
+++ ParentAid/CameraViewController.swift
@@ -10,7 +10,7 @@ import AlamofireImage
 import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var eventField: UITextField!
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
@@ -18,55 +18,55 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     @IBAction func onSubmitButton(_ sender: Any) {
-        // Create new object "post" that will be stored in table "Posts"
-                let event = PFObject(className: "KidsEvents")
-                //create arbitrary key "caption", "author"
-                event["synopsis"] = eventField.text!
-                //author of the post will be current user
+        // Create new object "event" that will be stored in table "KidsEvents"
+        let event = PFObject(className: "KidsEvents")
+        //Create arbitrary key "synopsis", "provider"
+        event["synopsis"] = eventField.text!
+        //Create provider of the event will be current user
         event["provider"] = PFUser.current()!
-                
-                
-                let imageData = imageView.image!.pngData()
-                let file = PFFileObject(data: imageData!)
+        
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(data: imageData!)
         event["image"] = file
-                //save the object ("post")to the table
+        //Save the object ("event")to the table
         event.saveInBackground{(success, error) in
-                    if success {
-                        self.dismiss(animated: true, completion: nil)
-                        print("Saved!!!")
-                    } else {
-                        print ("Error!!!")
-                    }
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("Saved!!!")
+            } else {
+                print ("Error!!!")
             }
-
+        }
+        
     }
     
     @IBAction func onCameraButton(_ sender: Any) {
         let picker = UIImagePickerController()
-                picker.delegate = self
-                picker.allowsEditing = true
-                //check if camera is available, if not then use photoLibrary
-                if UIImagePickerController.isSourceTypeAvailable(.camera){
-                    picker.sourceType = .camera
-                    
-                }else {
-                    picker.sourceType = .photoLibrary
-                }
-                present(picker, animated: true, completion: nil)
-                }
+        picker.delegate = self
+        picker.allowsEditing = true
+        //Check if camera is available, if not then use photoLibrary
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.sourceType = .camera
+            
+        }else {
+            picker.sourceType = .photoLibrary
+        }
+        present(picker, animated: true, completion: nil)
+    }
     //function to display image
-       func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-           let image = info [.editedImage] as! UIImage
-           let size = CGSize(width: 300, height: 300)
-            let scaledImage = image.af_imageAspectScaled(toFill: size)
-    
-    
-           
-           imageView.image = scaledImage
-           //dismiss camera view
-           dismiss(animated: true, completion: nil)
-       }
-       }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info [.editedImage] as! UIImage
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageAspectScaled(toFill: size)
+        
+        
+        
+        imageView.image = scaledImage
+        //dismiss camera view
+        dismiss(animated: true, completion: nil)
+    }
+}
 
 
-   
+
